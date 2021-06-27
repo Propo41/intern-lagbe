bfe1a1c54e4b331020d326eef58a642bd443357f
Propo41 <aliahnaf327@gmail.com>
2021-06-28 02:09:11 +0600

    - added pussies for matcovic sucker

--- client/src/containers/LandingPage/LandingPage.jsx
+++ client/src/containers/LandingPage/LandingPage.jsx
@@ -1,13 +1,49 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 import PublicNavbar from "../../components/PublicNavbar/PublicNavbar";
 import LandingPageImage from "../../assets/images/landing_page_image.svg";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
 
 const LandingPage = () => {
+  const useStyles = makeStyles(() => ({
+    landingImage: {
+      marginLeft: 50,
+      width: "90%",
+      height: "90%",
+    },
+
+    getStartedCard: {
+      padding: 30,
+      textAlign: "center",
+      marginTop: "65px",
+    },
+
+    buttonSmallPurple: {
+      backgroundColor: "var(--purple)",
+      color: "white",
+      fontFamily: "Sen",
+      marginTop: 10,
+      padding: "var(--button-padding)",
+    },
+
+    buttonSmallRed: {
+      backgroundColor: "var(--red)",
+      color: "white",
+      fontFamily: "Sen",
+      marginTop: 10,
+      padding: "var(--button-padding)",
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className="content-grid-padding">
       <PublicNavbar />
-      <Grid container className="content-grid-padding">
+      {/* Landing page */}
+      <Grid container>
         <Grid item xs>
           <h1 className="landing-page-title">Apply for your dream Job</h1>
           <h1 className="landing-page-subtitle">
@@ -19,7 +55,48 @@ const LandingPage = () => {
           </h1>
         </Grid>
         <Grid item xs>
-          <img src="/assets/images/landing_page_image.svg" alt="landing page" />
+          <img
+            src="/assets/images/landing_page_image.svg"
+            alt="landing page"
+            className={classes.landingImage}
+          />
+        </Grid>
+      </Grid>
+      {/* Landing page body */}
+      <Grid container>
+        <Grid item xs>
+          <h1
+            className="section-heading"
+            style={{
+              marginTop: "65px",
+            }}
+          >
+            FIND YOUR DREAM JOB
+          </h1>
+
+          <h1 className="landing-page-subtitle">A unified platfor</h1>
+        </Grid>
+
+        <Grid item xs>
+          <Paper elevation={2} className={classes.getStartedCard}>
+            <h1 className="section-heading">GET STARTED</h1>
+            <h1 className="content">Lorem ipsum dolor sit amet, consectetur</h1>
+            <Button
+              variant="contained"
+              fullWidth={true}
+              className={classes.buttonSmallPurple}
+            >
+              CREATE A RESUME
+            </Button>
+
+            <Button
+              variant="contained"
+              fullWidth={true}
+              className={classes.buttonSmallRed}
+            >
+              APPLYING TIPS
+            </Button>
+          </Paper>
         </Grid>
       </Grid>
     </div>

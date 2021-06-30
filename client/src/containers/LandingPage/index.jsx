c2f3089afc85a428f068d70629b1414184f835fa
Fahim Shahriyar <fahimshahriyar@gmail.com>
2021-06-30 19:55:21 +0600

    -- added toolbar v2

--- client/src/containers/LandingPage/index.jsx
+++ client/src/containers/LandingPage/index.jsx
@@ -7,6 +7,7 @@ import Button from "@material-ui/core/Button";
 import FilterBySort from "../../components/FilterBySort";
 import FilterByLocation from "../../components/FilterByLocation";
 import AvailPositionCard from "../../components/AvailPositionCard";
+import TextInputLayout from "../../components/TextInputLayout";
 
 const companies = [
   {
@@ -65,132 +66,137 @@ const LandingPage = () => {
   const classes = useStyles();
 
   return (
-    <div className="content-grid-padding">
+    <>
+      {/* Place Toolbar Here */}
       <PublicNavbar />
-      {/* Landing page */}
-      <Grid container>
-        <Grid item xs={12} sm={6}>
-          <h1 className="landing-page-title">Apply for your dream Job</h1>
-          <h1 className="landing-page-subtitle">
-            A unified platform dedicated to hosting internship job posts in
-            Bangladesh. Apply for your dream job directly from our website
-            without any hassle of searching everywhere. Simply search for your
-            desired job and apply to the job you like with your resume
-            completely for free!
-          </h1>
+      <div className="content-grid-padding">
+        {/* Landing page */}
+        <Grid container>
+          <Grid item xs={12} sm={6}>
+            <h1 className="landing-page-title">Apply for your dream Job</h1>
+            <h1 className="landing-page-subtitle">
+              A unified platform dedicated to hosting internship job posts in
+              Bangladesh. Apply for your dream job directly from our website
+              without any hassle of searching everywhere. Simply search for your
+              desired job and apply to the job you like with your resume
+              completely for free!
+            </h1>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <img
+              src="/assets/images/landing_page_image.svg"
+              alt="landing page"
+              className={classes.landingImage}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <img
-            src="/assets/images/landing_page_image.svg"
-            alt="landing page"
-            className={classes.landingImage}
-          />
-        </Grid>
-      </Grid>
-      {/* Landing page body */}
-      <Grid container spacing={5}>
-        <Grid
-          item
-          xs={12}
-          lg={8}
-          style={{
-            marginTop: "65px",
-          }}
-        >
-          <Grid container>
-            <Grid
-              item
-              xs={12}
-              sm={5}
-              style={{
-                alignSelf: "center",
-              }}
-            >
-              <h1
-                className="section-heading"
+        {/* Landing page body */}
+        <Grid container spacing={5}>
+          <Grid
+            item
+            xs={12}
+            lg={8}
+            style={{
+              marginTop: "65px",
+            }}
+          >
+            <Grid container>
+              <Grid
+                item
+                xs={12}
+                sm={5}
                 style={{
-                  margin: "0",
+                  alignSelf: "center",
                 }}
               >
-                FIND YOUR DREAM JOB
-              </h1>
+                <h1
+                  className="section-heading"
+                  style={{
+                    margin: "0",
+                  }}
+                >
+                  FIND YOUR DREAM JOB
+                </h1>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                sm={7}
+                style={{
+                  position: "relative",
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  zIndex: "1000",
+                }}
+              >
+                <FilterBySort />
+                <FilterByLocation />
+              </Grid>
             </Grid>
-            <Grid
-              item
-              xs={12}
-              sm={7}
+            <div
               style={{
-                position: "relative",
-                display: "flex",
-                justifyContent: "flex-end",
-                zIndex: "1000",
+                marginTop: "30px",
               }}
-            >
-              <FilterBySort />
-              <FilterByLocation />
-            </Grid>
+            ></div>
+
+            {companies.map((company, index) => {
+              return (
+                <AvailPositionCard
+                  expandable={false}
+                  company={company.company}
+                  address={company.address}
+                  disabledButton={false}
+                />
+              );
+            })}
           </Grid>
-          <div
-            style={{
-              marginTop: "30px",
-            }}
-          ></div>
 
-          {companies.map((company, index) => {
-            return (
-              <AvailPositionCard
-                expandable={false}
-                company={company.company}
-                address={company.address}
-                disabledButton={false}
-              />
-            );
-          })}
+          <Grid item xs={12} lg={4}>
+            <Paper elevation={2} className={classes.getStartedCard}>
+              <h1 className="section-heading">GET STARTED</h1>
+              <h1 className="content">
+                Lorem ipsum dolor sit amet, consectetur
+              </h1>
+              <Button
+                variant="contained"
+                fullWidth={true}
+                className={classes.buttonSmallPurple}
+                style={{ marginTop: "var(--margin-item-spacing-lg)" }}
+              >
+                CREATE A RESUME
+              </Button>
+
+              <Button
+                variant="contained"
+                fullWidth={true}
+                className={classes.buttonSmallRed}
+              >
+                APPLYING TIPS
+              </Button>
+            </Paper>
+
+            <Paper elevation={2} className={classes.subscriptionCard}>
+              <h1 className="section-heading">SUBSCRIBE TO NEWSLETTER</h1>
+              <div style={{ marginTop: "var(--margin-item-spacing-lg)" }}>
+                <TextInputLayout
+                  icon="mail"
+                  placeholder="Enter your email"
+                  type="mail"
+                />
+              </div>
+              <Button
+                variant="contained"
+                fullWidth={true}
+                className={classes.buttonSmallPurple}
+                style={{ marginTop: "var(--margin-item-spacing-lg)" }}
+              >
+                SUBSCRIBE
+              </Button>
+            </Paper>
+          </Grid>
         </Grid>
-
-        <Grid item xs={12} lg={4}>
-          <Paper elevation={2} className={classes.getStartedCard}>
-            <h1 className="section-heading">GET STARTED</h1>
-            <h1 className="content">Lorem ipsum dolor sit amet, consectetur</h1>
-            <Button
-              variant="contained"
-              fullWidth={true}
-              className={classes.buttonSmallPurple}
-              style={{ marginTop: "var(--margin-item-spacing-lg)" }}
-            >
-              CREATE A RESUME
-            </Button>
-
-            <Button
-              variant="contained"
-              fullWidth={true}
-              className={classes.buttonSmallRed}
-            >
-              APPLYING TIPS
-            </Button>
-          </Paper>
-
-          <Paper elevation={2} className={classes.subscriptionCard}>
-            <h1 className="section-heading">SUBSCRIBE TO NEWSLETTER</h1>
-            <div style={{ marginTop: "var(--margin-item-spacing-lg)" }}>
-              <TextInputLayout
-                icon="mail"
-                placeholder="Enter your email"
-                type="mail"
-              />
-            </div>
-            <Button
-              variant="contained"
-              fullWidth={true}
-              className={classes.buttonSmallPurple}
-              style={{ marginTop: "var(--margin-item-spacing-lg)" }}
-            >
-              SUBSCRIBE
-            </Button>
-          </Paper>
-        </Grid>
-      </Grid>
-    </div>
+      </div>
+    </>
   );
 };
 

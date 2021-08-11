560180ca3ed4b790eb3ffa866dc7435b08e04c15
Fahim Shahriyar <fahimshahriyar@gmail.com>
2021-08-11 09:23:10 +0600

    - error page added

---
+++ client/src/styles/error_page.js
@@ -0,0 +1,35 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: "6rem",
+    flexWrap: "wrap",
+    alignItems: "center",
+  },
+  image: {
+    justifyContent: "center",
+    width: "70%",
+    height: "70%",
+    [theme.breakpoints.down("lg")]: {
+      width: "90%",
+      height: "90%",
+    },
+  },
+  buttonPurple: {
+    backgroundColor: "var(--purple)",
+    color: "white",
+    fontFamily: "Sen",
+    marginTop: 10,
+    padding: "var(--button-padding)",
+    fontSize: "var(--font-size-button-small)",
+  },
+  title: {
+    fontSize: "5rem",
+  },
+  description: {
+    fontSize: "var(--font-size-title)",
+    fontWeight: 200,
+  },
+}));
+
+export default useStyles;

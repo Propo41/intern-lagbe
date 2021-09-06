7626360f7b56ca77fddb64beb6bbbc150117a40e
Propo41 <aliahnaf327@gmail.com>
2021-09-06 22:49:05 +0600

    secured middleware for admin role

--- Helpers/AuthorizationAttribute.cs
+++ Helpers/AuthorizationAttribute.cs
@@ -22,7 +22,15 @@ namespace InternFinder.Helpers
             // authorization
             var user = (User)context.HttpContext.Items["User"];
             if (user == null)
+            {
                 context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+
+            }
+            if (user.Role != "Admin")
+            {
+                context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+
+            }
         }
     }
 }
